01609045f8ee790ba7798bfa196d228306da1d3e
pooja-hub200 <68578996+pooja-hub200@users.noreply.github.com>
2022-09-09 10:36:52 +0530

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,15 @@
+package com.lulu.io;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ScannerBasics a=new ScannerBasics();
+		System.out.println("si is "+a.si());
+		//a.oddoreven();
+		
+		
+
+	}
+
+}
