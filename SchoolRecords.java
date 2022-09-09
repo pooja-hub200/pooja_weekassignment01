32a63d54f6f86c7b2c1c254fbabf3d552b826c86
pooja-hub200 <68578996+pooja-hub200@users.noreply.github.com>
2022-09-09 10:33:17 +0530

    Create SchoolRecords.java

---
+++ SchoolRecords.java
@@ -0,0 +1,23 @@
+package com.lulu.oopbasics;
+import java.lang.*;
+
+public class SchoolRecords {
+	
+	int School_id;
+	String school_name;
+	String school_adress;
+	
+	//creating method to add all records 
+	void addRecords()
+	{
+		System.out.println("Adding records");
+		System.out.println("Record 1,Record 2...");
+	}
+	//creating method to displaying all records and returning the values
+	int dispRecords()
+	{
+		return 1;
+	}
+	
+
+}
