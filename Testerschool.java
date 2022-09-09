37f20887d8f434acd19777e7b6140ea027ca567c
pooja-hub200 <68578996+pooja-hub200@users.noreply.github.com>
2022-09-09 10:33:52 +0530

    Create Testerschool.java

---
+++ Testerschool.java
@@ -0,0 +1,12 @@
+package com.lulu.oopbasics;
+
+public class Testerschool {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SchoolRecords sr=new SchoolRecords();
+		sr.addRecords();
+		System.out.println("Displaying records"+sr.dispRecords());
+	}
+
+}
